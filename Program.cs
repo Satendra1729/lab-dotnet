6c000b01737d32d4632ef171264c25ccb9930735
satendra1729 <satendra1729@gmail.com>
2022-11-17 19:37:33 +0530

    setup configuration

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿
 
 
+using Microsoft.Extensions.Configuration;
+
 namespace cli; 
 
 public class Program {
 
-
+    const string ENV_PREFIX = "CLI_TOOL_"; 
     public static void Main(string[] args){
-        Console.WriteLine("From Main Method !!"); 
+        var builder = new ConfigurationBuilder(); 
+
+        var config = builder.AddJsonFile("appsettings.json",false,true)
+
+                    .AddEnvironmentVariables(prefix: ENV_PREFIX)
+
+                    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("CLI_TOOL_ENV")}.json",true,true).Build(); 
+
+        var envInfo = new EnvInfo(); 
+
+        config.Bind("EnvInfo",envInfo); 
+        
+        Console.WriteLine(envInfo.greeting); 
+
+        // export CLI_TOOL_EnvInfo__machine="machine name"   ::  prefix plus key value sperated by double undersore  
+        Console.WriteLine(envInfo.machine); 
     }
+}
+
+public class EnvInfo {
+    public string env {get;set;}
+
+    public string greeting {get;set;}
+
+    public string machine {get;set;}
 }
